031044f8c2e487fa95d2c10bf02129a41f2bf54a
ioann <ivan.ivenskiy97@mail.ru>
2025-01-27 17:50:51 +0300

    Добавьте файлы проекта.

---
+++ unior/permenne.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unior
+{
+    public class permenne
+    {
+        public static void Main()
+        {
+            int age = 27;
+            double version = 1.2;
+            string name = "Ioann";
+            string surname = "Ivenskiy";
+            char gender = 'M';
+            string[] fullName = { "Ioann", "Ivenskiy", "Olegovich" };
+            bool islike = false;
+            UInt32 numPositiv = 1000;
+            byte numTiny = 2;
+            long numLarge = 2024100110250120;
+        }
+    }
+}
