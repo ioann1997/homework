b89ed303f40fef45822e443582536171f9133a08
ioann1997 <ivan.ivenskiy97@mail.ru>
2025-01-27 20:24:07 +0300

    Update Page.cs

--- unior/Page.cs
+++ unior/Page.cs
@@ -11,9 +11,9 @@ namespace unior
         public static void Page1()
         { 
             int numLine = int.Parse(Console.ReadLine()); //3
-            int Total = int.Parse(Console.ReadLine()); //52
-            int result = Total/ numLine;
-            int leftOver = Total%numLine;
+            int total = int.Parse(Console.ReadLine()); //52
+            int result = total/ numLine;
+            int leftOver = total%numLine;
             Console.WriteLine("полностью заполненные ряды {0} " +
                 " картинок  сверх меры {1}", result, leftOver);
         }
