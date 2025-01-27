0d2ed94ac2b1e1176b54cda0daf00bec517c92a7
ioann1997 <ivan.ivenskiy97@mail.ru>
2025-01-27 20:21:40 +0300

    Update Str.cs

--- unior/Str.cs
+++ unior/Str.cs
@@ -4,19 +4,18 @@ public class Str
 {
     public static void Stri()
     {
+        char genderMan = 'М';
+        char genderWomen = 'Ж';
         Console.Write("Введите своё Имя ");
         string name = Console.ReadLine();
-        Console.Write("Введите свой пол М/Ж ");
+        Console.Write($"Введите свой пол {genderMan}/{genderWomen} ");
         char gender = char.Parse(Console.ReadLine());
         Console.Write("Сколько вам лет? ");
         string age = Console.ReadLine();
-        if (gender == 'М')
+        if (gender == genderMan)
             Console.WriteLine("Приветсвую, Добрый путник {0} возрастом таким-то {1}", name, age);
-        else if (gender == 'Ж')
+        else if (gender == genderWomen)
             Console.WriteLine("Приветсвую, Добрую путницу {0} возрастом таким-то {1}", name, age);
-
-
-
     }
 
 }
