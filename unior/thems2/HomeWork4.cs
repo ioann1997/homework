63e661eb0b5ffb163b0a2ac68b8a6112f8695d7d
ioann <ivan.ivenskiy97@mail.ru>
2025-01-28 18:53:28 +0300

    исправил 4 задачу

--- unior/thems2/HomeWork4.cs
+++ unior/thems2/HomeWork4.cs
@@ -10,14 +10,16 @@ namespace unior
 {
     internal class HomeWork4
     {
-        public static void  Hm4() 
+        public static void Hm4()
         {
             Random random = new Random();
-            int max = 101;
+
+            int max = 100;
             int numDivision1 = 3;
             int numDivision2 = 5;
             int number = random.Next(max);
-                for(int i = 3; i < number; i++)
+
+            for (int i = 0; i <= number; i++)
             {
                 if (i % numDivision1 == 0 || i % numDivision2 == 0)
                     Console.WriteLine(i);
