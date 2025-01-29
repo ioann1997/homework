698b57e02d7341f0b82637ee81463849adf09274
ioann <ivan.ivenskiy97@mail.ru>
2025-01-29 20:08:05 +0300

    xvxg

---
+++ unior/Thems2/HomeWork10.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Unior
+{
+    internal class HomeWork10
+    {
+        public static void Hm10()
+        {
+            Random random = new Random();
+            int startRandomNumber = 1;
+            int endRandomNumber = 100;
+            int numberBase = 3;
+            int number = random.Next(startRandomNumber, endRandomNumber+1); 
+            int power = 0;
+            int result = 1; 
+
+            while (result <= number)
+            {
+                result *= numberBase; 
+                power++; 
+            }
+            Console.WriteLine($"Заданное число: {number}");
+            Console.WriteLine($"Минимальная степень: {numberBase}^{power} = {result}");
+        }
+    }
+}
---
+++ unior/Thems2/HomeWork9.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Unior
+{
+    class HomeWork9
+    {
+        public static void Hm9()
+        {
+            Random random = new Random();
+            int startRandomNumber = 10;
+            int endRandomNumber = 25;
+            int startNumber = 50;
+            int endNumber = 150;
+            int number = random.Next(startRandomNumber, endRandomNumber+1); 
+            int counter = 0;
+
+            Console.WriteLine($"Сгенерированное значение number: {number}");
+
+            for (int i = number; i <= endNumber; i+=number)
+            {
+                if (i >= startNumber)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine($"Количество чисел от {startNumber} до {endNumber}, кратных {number}: {counter}");
+        }
+    }
+}
