319620c2cdb789d0038de3cd26c1f07030fd5823
ioann <ivan.ivenskiy97@mail.ru>
2025-02-01 12:33:20 +0300

    hm1

---
+++ unior/Thems3/HomeWork1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Unior
+{
+    internal class HomeWork1
+    {
+        public static void Hm1()
+        {
+            int[,] array =
+            {
+                { 1, 2, 3, 4, 5, },
+                { 6, 7, 8, 9, 0, },
+                { 2, 4, 6, 8, 3, }
+            };
+
+            int sumSecondRow = 0;
+            int resultFirstСolumn = 1;
+            int rows = array.GetLength(0);
+            int columns = array.GetLength(1);
+
+            for (int j = 0; j < columns; j++)
+            {
+                sumSecondRow += array[1, j];
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                resultFirstСolumn *= array[i, 0];
+            }
+
+            Console.WriteLine($"Сумма второй строки - {sumSecondRow}, произведение первого столбца {resultFirstСolumn}");
+        }
+    }
+}
