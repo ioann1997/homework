7a93ad55eef1b336539f9c15d3b2701ba3f5cdd1
ioann <ivan.ivenskiy97@mail.ru>
2025-02-02 22:22:46 +0300

    hm

--- unior/Thems3/HomeWork3.cs
+++ unior/Thems3/HomeWork3.cs
@@ -4,7 +4,7 @@ namespace Unior
 {
     internal class HomeWork3
     {
-        public static void Hm3()
+        public static void Main()
         {
             Random random = new Random();
 
@@ -16,37 +16,26 @@ namespace Unior
             for (int i = 0; i < numbers.Length; i++)
             {
                 numbers[i] = random.Next(minRandomNumber, maxRandomNumber + 1);
-            }
-
-            for (int i = 0; i < numbers.Length; i++)
-            {
                 Console.Write(numbers[i] + " ");
             }
 
             Console.WriteLine();
 
-            for (int i = 0; i < numbers.Length; i++)
+            if (numbers[0] > numbers[1])
             {
-                if (i == 0)
+                Console.Write(numbers[0] + " ");
+            }
+
+            if (numbers[^1] > numbers[^2])
+            {
+                Console.Write(numbers[^1] + " ");
+            }
+
+            for (int i = 1; i < numbers.Length-1; i++)
+            {
+                if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
                 {
-                    if (numbers[i] > numbers[i + 1])
-                    {
-                        Console.Write(numbers[i] + " ");
-                    }
-                }
-                else if (i == numbers.Length - 1)
-                {
-                    if (numbers[i] > numbers[i - 1])
-                    {
-                        Console.Write(numbers[i] + " ");
-                    }
-                }
-                else 
-                {
-                    if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
-                    {
-                        Console.Write(numbers[i] + " ");
-                    }
+                    Console.Write(numbers[i] + " ");
                 }
             }
         }
