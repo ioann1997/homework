8bb5cec4ab3a9582b6200d603a1655803fcf8b26
ioann <ivan.ivenskiy97@mail.ru>
2025-02-05 16:49:51 +0300

    9

---
+++ unior/Thems3/HomeWork9.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Unior.Thems3
+{
+    internal class HomeWork9
+    {
+        public static void Main()
+        {
+            string staples = "(()(()))";
+            int currentDepth = 0;
+            int maxDeep = 0;
+            bool isValid = true;
+
+            for (int i = 0; i < staples.Length; i++)
+            {
+                if (staples[i] == '(')
+                {
+                    currentDepth++;
+                    maxDeep = currentDepth > maxDeep ? currentDepth : maxDeep;
+                }
+                else
+                {
+                    currentDepth--;
+
+                    if (currentDepth < 0)
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+            }
+
+            if (currentDepth == 0 && isValid)
+            {
+                Console.WriteLine($"Корректная строка, максимальная глубина - {maxDeep}");
+            }
+            else
+            {
+                Console.WriteLine("Некорректная строка");
+            }
+        }
+    }
+}
