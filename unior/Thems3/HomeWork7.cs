bb04c42db400a0aea8920c5e6ceb923ee308bd40
ioann <ivan.ivenskiy97@mail.ru>
2025-02-05 16:34:37 +0300

    78

---
+++ unior/Thems3/HomeWork7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Unior.Thems3
+{
+    internal class HomeWork7
+    {
+        public static void Hm7()
+        {
+            string[] input = Console.ReadLine().Split();
+
+            foreach (string word in input) 
+            { 
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ unior/Thems3/HomeWork8.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Unior.Thems3
+{
+    internal class HomeWork8
+    {
+        public static void Hm8()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 ,6,7,8};
+            Console.WriteLine(string.Join(", ", numbers));
+
+            int shift = int.Parse(Console.ReadLine());
+
+            shift = shift > numbers.Length ? shift % numbers.Length : shift;
+
+            Array.Reverse(numbers, 0, shift);
+            Array.Reverse(numbers, shift, numbers.Length-shift);
+            Array.Reverse(numbers);
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
