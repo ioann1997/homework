0430b9ddc6c2b3d75d5c29d5d912549547566402
ioann1997 <ivan.ivenskiy97@mail.ru>
2025-02-06 17:22:04 +0300

    Update HomeWork2.cs

--- unior/Thems4/HomeWork2.cs
+++ unior/Thems4/HomeWork2.cs
@@ -6,11 +6,11 @@ namespace Unior.Thems4
     {
         public static void Hm2()
         {
-            ShowUIElement(30, 20, "Healthbar");
-            ShowUIElement(60, 10, "Manabar");
+            ShowBar(30, 20, "Healthbar");
+            ShowBar(60, 10, "Manabar");
         }
 
-        public static void ShowUIElement(double percent, int lenght, string nameBar)
+        public static void ShowBar(double percent, int lenght, string nameBar)
         {
             int shaded = (int)Math.Round(percent / 100 * lenght);
             int unpainted = lenght - shaded;
