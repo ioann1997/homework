d2218df49ba4e9aecd615bb3cd5d2852033beed2
ioann <ivan.ivenskiy97@mail.ru>
2025-02-13 15:31:22 +0300

    5

---
+++ unior/Thems5/HomeWork5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unior.Thems5
+{
+    internal class HomeWork5
+    {
+        public static void Main()
+        {
+            string[] numbers1 = { "1", "2", "1" };
+            string[] numbers2 = { "3", "2" };
+
+            List<string> result = new List<string>();
+
+            string[] numbersUnion = new string[numbers1.Length + numbers2.Length];
+            numbers1.CopyTo(numbersUnion, 0);
+            numbers2.CopyTo(numbersUnion, numbers1.Length);
+
+            foreach (string number in numbersUnion)
+            {
+                if (result.Contains(number) == false)
+                { 
+                    result.Add(number);
+                }
+            }
+
+            foreach (string number in result)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
